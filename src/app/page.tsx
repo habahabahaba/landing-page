a9e3c56623e62033b2e98118f32c03027d53eec2
Alex S <habahabahaba@outlook.com>
2024-08-20 06:24:28 +0400

    Added nav-links to the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+// 3rd party:
+// Redux RTK:
+// Store:
+// React:
+// Next:
+import Link from 'next/link';
+// Context:
+// Components:
+// CSS:
+// Types, interfaces and enumns:
+import { FC } from 'react';
+
+const Home: FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
     </div>
   );
-}
+};
+export default Home;
